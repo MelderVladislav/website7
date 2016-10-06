cd91a15c207675931c96d3d5b805ddf665aacae6
MelderVladislav <melder.vladislav@gmail.com>
2016-10-06 19:04:52 +0300

    убрал ошибку незакрывающегося дива, добавил линию, добавил масштабируемый шрифт

--- Scripts/ChangePhoto.js
+++ Scripts/ChangePhoto.js
@@ -3,14 +3,15 @@ window.onload = loader;
 function loader()
 {
     var Counter = 0;
-    
-    var PictureMassiv = ["/Resourses/photos/001.jpg", 
-	"/Resourses/photos/002.jpg", "/Resourses/photos/101.jpg", 
-	"/Resourses/photos/102.jpg", "/Resourses/photos/201.jpg", "/Resourses/photos/202.jpg"];
-	/*
-    var PictureMassiv = ["Resourses/photos/001.jpg", 
-	"Resourses/photos/002.jpg", "Resourses/photos/101.jpg", 
-	"Resourses/photos/102.jpg", "Resourses/photos/201.jpg", "Resourses/photos/202.jpg"];*/
+   /*
+    var PictureMassiv = ["/Resources/photos/001.jpg",
+	"/Resources/photos/002.jpg", "/Resources/photos/101.jpg",
+	"/Resources/photos/102.jpg", "/Resources/photos/201.jpg", "/Resources/photos/202.jpg"];
+	 */
+
+    var PictureMassiv = ["Resources/photos/001.jpg",
+	"Resources/photos/002.jpg", "Resources/photos/101.jpg",
+	"Resources/photos/102.jpg", "Resources/photos/201.jpg", "Resources/photos/202.jpg"];
     var ImgNumber = PictureMassiv.length;
     var Image = document.getElementById("logoImg");
     Image.src = PictureMassiv[Counter];
@@ -33,20 +34,20 @@ function loader()
 		if (e.source!=Image)
 			ReducePhoto();
 	}
-	
+
 			function IncreasePhoto(e)
 		{
 			if(isBig===false)
 			{
-				
+
 		Image.style.width="600px";
 		Image.style.height="420px";
 		isBig=true;
 			}
-			else  
+			else
 				NextPhoto();
 			e.stopPropagation();
-			
+
 		}
 		function ReducePhoto(e)
 		{
@@ -56,55 +57,55 @@ function loader()
 		Image.style.height="270px";
 		currentWidth=400;
 		currentHeight=270;
-		isBig=false;	
-			
+		isBig=false;
+
 			}
-			
-			
+
+
 		}
 	function IncreasePhoto2(e)
 	{
 		if(isBig===false)
 		{
-				
+
 			timer=setInterval(Increase, 4);
-		
+
 			function Increase()
 				{
-							
+
 							if (currentHeight>=420 || currentWidth>=600)
-							{	
+							{
 							clearInterval(timer);
 							isBig=true;
 							}
-							else 
+							else
 								{
 								currentHeight+=velocity*usualRel;
 								currentWidth+=velocity;
 								Image.style.width=currentWidth+"px";
-								Image.style.height=currentHeight+"px";	
+								Image.style.height=currentHeight+"px";
 								}
-				
-						
+
+
 				}
-		
+
 		}
-			
-		else  
+
+		else
 		NextPhoto(e);
 	e.stopPropagation();
-		
+
 	}
-	
+
     function NextPhoto(e)
     {
-      
+
         Counter = Counter + 1;
         if (Counter === PictureMassiv.length) Counter = 0;
         Image.src = PictureMassiv[Counter];
 		e.stopPropagation();
     }
-	
+
     function PreviousPhoto(e)
     {
         if (Counter === 0) Counter = ImgNumber - 1;
